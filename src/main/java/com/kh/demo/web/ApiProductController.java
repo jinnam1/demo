5312f129a819c4301da6c03a6e23acd2e2896198
jinnam1 <rladbwls1795@gmail.com>
2024-11-07 11:10:31 +0900

    20241107-2

--- src/main/java/com/kh/demo/web/ApiProductController.java
+++ src/main/java/com/kh/demo/web/ApiProductController.java
@@ -3,7 +3,9 @@ package com.kh.demo.web;
 import com.kh.demo.domain.entity.Product;
 import com.kh.demo.domain.product.svc.ProductSVC;
 import com.kh.demo.web.api.ApiResponse;
+import com.kh.demo.web.req.product.ReqDels;
 import com.kh.demo.web.req.product.ReqSave;
+import com.kh.demo.web.req.product.ReqUpdate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -78,4 +80,60 @@ public class ApiProductController {
 
     return res;
   }
+
+  // 단건 삭제
+  @DeleteMapping("/{pid}")  // 요청 메소드 DELETE http://localhost:9080/api/products/301
+  public ApiResponse<String> delete(@PathVariable("pid") Long pid){
+    ApiResponse<String> res = null;
+
+    int rows = productSVC.deleteById(pid);  //반환 삭제 건수
+    if(rows == 1){
+      res = ApiResponse.createApiResponse("00", "success",null);
+    }else{
+      res = ApiResponse.createApiResponse("01", "not found",null);
+    }
+
+    return res;
+  }
+
+  // 수정
+  @PatchMapping("{pid}")
+  public ApiResponse<Product> update(@PathVariable("pid") Long pid,
+                            @RequestBody ReqUpdate reqUpdate){
+
+    log.info("pid = {} ,reqUpdate = {}", pid,reqUpdate);
+    ApiResponse<Product> res = null;
+
+    Product product = new Product();
+    BeanUtils.copyProperties(reqUpdate, product);
+
+
+    int rows = productSVC.updateById(pid,product);
+    if(rows == 1){
+      Product updateProduct = productSVC.findByID(pid).get();
+      res = ApiResponse.createApiResponse("00", "success",updateProduct);
+    }else{
+      res = ApiResponse.createApiResponse("01", "not found",null);
+    }
+
+
+   return res;
+  }
+
+  // 여러 건 삭제
+  @DeleteMapping
+  public ApiResponse<String> deleteByIds(@RequestBody ReqDels reqDels){
+    log.info("reqDels = {}", reqDels);
+    ApiResponse<String> res = null;
+
+    int rows = productSVC.deleteByIds(reqDels.getProductIds());
+    if(rows > 0){
+      res = ApiResponse.createApiResponse("00", "success","삭제건수 : " + rows);
+    }else{
+      res = ApiResponse.createApiResponse("01", "not found",null);
+    }
+
+    return res;
+  }
+
 }
---
+++ src/main/java/com/kh/demo/web/req/product/ReqUpdate.java
@@ -0,0 +1,15 @@
+package com.kh.demo.web.req.product;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class ReqUpdate {
+  private String pname;
+  private Long price;
+  private Long quantity;
+}
